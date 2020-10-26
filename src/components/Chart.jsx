b2b5f100b5d915e6cf4ab981fc84a87f0c9ffc6e
TheAlmightyCrumb <67858097+TheAlmightyCrumb@users.noreply.github.com>
2020-10-26 18:06:36 +0200

    Created charts and graphs

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,17 +1,74 @@
-import { LineChart, Line, CartesianGrid, XAxis, YAxis } from "recharts";
+import {
+  Legend,
+  Area,
+  AreaChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+} from "recharts";
 
-export default function Chart() {
-  const data = [{ name: "Page A", uv: 400, pv: 2400, amt: 2400 }];
+export default function Chart(props) {
+  const { firstChoice, secondChoice } = props;
+  const firstName = firstChoice["Province/State"]
+    ? firstChoice["Province/State"]
+    : firstChoice["Country/Region"];
+  const secondName = secondChoice["Province/State"]
+    ? secondChoice["Province/State"]
+    : secondChoice["Country/Region"];
+
+  const newDataArray = [];
+
+  for (const [key, value] of Object.entries(firstChoice)) {
+    let date = new Date(key);
+    if (date != "Invalid Date") {
+      let newObj = {
+        date: key,
+        [firstName]: parseInt(value),
+        [secondName]: parseInt(secondChoice[key]),
+      };
+      newDataArray.push(newObj);
+    }
+  }
 
   return (
-    <div>
-      <LineChart width={500} height={300} data={data}>
-        <XAxis dataKey="name" />
+    <section id="chart-section">
+      <AreaChart
+        width={800}
+        height={400}
+        data={newDataArray}
+        margin={{ top: 0, right: 25, left: 60, bottom: 0 }}
+      >
+        <defs>
+          <linearGradient id="colorFirst" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="10%" stopColor="navy" stopOpacity={0.8} />
+            <stop offset="95%" stopColor="navy" stopOpacity={0} />
+          </linearGradient>
+          <linearGradient id="colorSecond" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="10%" stopColor="maroon" stopOpacity={0.8} />
+            <stop offset="95%" stopColor="maroon" stopOpacity={0} />
+          </linearGradient>
+        </defs>
+        <XAxis dataKey="date" />
         <YAxis />
-        <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
-        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-        <Line type="monotone" dataKey="pv" stroke="#82ca9d" />
-      </LineChart>
-    </div>
+        <CartesianGrid strokeDasharray="3 3" />
+        <Tooltip />
+        <Legend verticalAlign="bottom" height={50} />
+        <Area
+          type="monotone"
+          dataKey={firstName}
+          stroke="navy"
+          fillOpacity={1}
+          fill="url(#colorFirst)"
+        />
+        <Area
+          type="monotone"
+          dataKey={secondName}
+          stroke="maroon"
+          fillOpacity={1}
+          fill="url(#colorSecond)"
+        />
+      </AreaChart>
+    </section>
   );
 }
